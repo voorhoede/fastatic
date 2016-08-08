0bd7ce15b3688facee6b0d5c84a0064a5403e8de
jpsc <joao@voorhoede.nl>
2016-08-08 14:28:41 +0200

    feat(examples): change paths on react examples
    #48

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require("gulp");
+const replace = require("gulp-replace");
+
+gulp.task('examples_react', function() {
+	return gulp.src('./dist/examples/react/**/*.html')
+		.pipe(replace('/react/', '/examples/react/'))
+		.pipe(gulp.dest('./dist/examples/react'))
+});
+
+
