6e45ea0e12bcb481b4a149df8480c99247b0d6bb
jpsc <joao@voorhoede.nl>
2016-08-08 14:41:06 +0200

    feat(examples): remove general gulpfile and move task to scripts
    #48

--- scripts/post-react.js
+++ scripts/post-react.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp");
 const replace = require("gulp-replace");
 
-gulp.task('examples_react', function() {
+gulp.start('examples_react', function() {
 	return gulp.src('./dist/examples/react/**/*.html')
 		.pipe(replace('/react/', '/examples/react/'))
 		.pipe(gulp.dest('./dist/examples/react'))
