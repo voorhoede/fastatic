a138f99987477897b653f6b2edf9bf26b6189ce3
João Carmona <jpsc@users.noreply.github.com>
2016-08-19 17:00:54 +0200

    Revert "feat(ci): publish disto to gh-pages on sucessfull build"

--- scripts/replace-react-paths-travis.js
+++
@@ -1,9 +0,0 @@
-const gulp = require("gulp");
-const replace = require("gulp-replace");
-
-gulp.src('./build/examples/react/**/*.html')
-	.pipe(replace('/examples/react/', '/fastastic/examples/react/'))
-	.pipe(gulp.dest('./build/examples/react'))
-
-
-
