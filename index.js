8fe20537e83d1de244b6da93ce8b123d75f6c68d
Peter Goes <petergoes@gmail.com>
2016-09-08 15:10:26 +0200

    refactor(index): remove old stats module

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const compareFileSize = require('./lib/compare-file-size');
 const copy = require('./lib/copy');
 const defineConfig = require('./lib/define-config');
-const Logger = require('./lib/logger');
 const parseAll = require('./lib/parse-all');
 const promisify = require('bluebird').promisify;
 const remove = promisify(require('rimraf'));
 const Spinner = require('cli-spinner').Spinner;
-const stats = require('./lib/parser-stats');
 
 function fastatic(options) {
 	const config = defineConfig(options);
-	const logger = new Logger(options.logLevel);
 
 	const result = Promise.all([
 			copy(config.src, config.temp.src),
@@ -18,8 +15,6 @@ function fastatic(options) {
 		])
 		.then(() => parseAll(config))
 		.then(() => copy(config.temp.dest, config.dest))
-		.then(() => stats(config))
-		.then(output => logger.log(output))
 		.then(() => compareFileSize(config.temp.src, config.temp.dest))
 		.then(fileSize => ({fileSize}))
 		.catch(err => {
