8547d59bcbc2c7a322d900533da30eb3e728349d
Peter Goes <petergoes@gmail.com>
2016-09-15 12:00:30 +0200

    Define loglevel once (#98)
    * refactor(logger): make loglevel static and shared accross instances
    
    * refactor(fastatic): remove unneeded loglevel argument
    
    * refactor(logger): spread arguments in apply method
    
    * refactor(logger): replace class with a normal object
    
    Closed #96

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const compareFileSize = require('./lib/compare-file-size');
 const copy = require('./lib/copy');
 const defineConfig = require('./lib/define-config');
+const logger = require('./lib/logger');
 const parseAll = require('./lib/parse-all');
 const promisify = require('bluebird').promisify;
 const remove = promisify(require('rimraf'));
 
 function fastatic(options) {
 	const config = defineConfig(options);
+
+	logger.setLogLevel(options.logLevel);
 
 	const result = Promise.all([
 			copy(config.src, config.temp.src),
