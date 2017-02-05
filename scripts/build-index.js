151e16dce2e6be5dfd6e1cfdb36aa418de066901
Sanne Veroude <veroude.sanne@gmail.com>
2017-02-05 19:48:17 +0100

    chore(build): format readme as HTML page using formatter (#124)
    * chore(build): format readme as HTML page using
    * chore(package.json): update voorhoede-ocelot-formatter to the latest version
    * chore(build-index): add link to github

---
+++ scripts/build-index.js
@@ -0,0 +1,17 @@
+const formatter = require('voorhoede-ocelot-formatter');
+const fs = require('fs');
+const pkg = require('../package.json');
+const rebaseRelativeUrls = require('../lib/rebase-relative-urls');
+
+const masterBranchUrl = pkg.repository.url + '/blob/master/';
+const outputFilename = 'build/index.html';
+
+fs.readFile('README.md', 'utf8', (err, readme) => {
+	if (err) {
+		console.error('Error reading README.md', err);
+		return;
+	}
+	formatter(readme, { github: 'voorhoede/fastatic' })
+		.then(html => rebaseRelativeUrls(html, masterBranchUrl))
+		.then(html => fs.writeFile(outputFilename, html));
+});
